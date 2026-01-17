812a004debda2683cc1447255d4310a5c9e4ccf9
JPrevilon <joshuaprevilon13@gmail.com>
2026-01-17 16:52:47 -0500

    Complete scope lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+// Global scope variables
+const burgers = ["Hamburger", "Cheeseburger"];
+let featuredDrink = "Strawberry Milkshake";
+
+function addBurger() {
+  // Function-scoped variable
+  const newBurger = "Flatburger";
+  burgers.push(newBurger);
+}
+
+if(true) {
+  // Block-scoped variable
+  const anotherNewBurger = "Maple Bacon Burger";
+  burgers.push(anotherNewBurger);
+}
+
+function changeFeaturedDrink() {
+  featuredDrink = "The JavaShake";
+}
